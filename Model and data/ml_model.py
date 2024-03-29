12d8ef9a8605e19ac961db07b689512f644d8e4d
raj208 <rajendrajfc1021@gmail.com>
2024-03-29 20:03:37 +0530

    trained by new model

--- Model and data/ml_model.py
+++ Model and data/ml_model.py
@@ -1,34 +1,50 @@
 import pickle
 import pandas as pd
 import numpy as np
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
 
-dataset = pd.read_csv('train.csv', encoding='latin-1')
-dataset = dataset.rename(columns=lambda x: x.strip().lower())
-dataset.head()
+# Load the dataset
+train = pd.read_csv('titanic_train.csv')
 
-dataset = dataset[['pclass', 'sex', 'age', 'sibsp',
-                   'parch', 'fare', 'embarked', 'survived']]
-dataset['sex'] = dataset['sex'].map({'male': 0, 'female': 1})
-dataset['age'] = pd.to_numeric(dataset['age'], errors='coerce')
-dataset['age'] = dataset['age'].fillna(np.mean(dataset['age']))
+# Preprocessing data (cleaning data)
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(Age):
+        if Pclass == 1:
+            return 38
+        elif Pclass == 2:
+            return 29
+        else:
+            return 25
+    else:
+        return Age
 
-embarked_dummies = pd.get_dummies(dataset['embarked'])
-dataset = pd.concat([dataset, embarked_dummies], axis=1)
-dataset = dataset.drop(['embarked'], axis=1)
+train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
+train.drop('Cabin', axis=1, inplace=True)
 
-X = dataset.drop(['survived'], axis=1)
-y = dataset['survived']
+# Convert categorical variables into dummy variables
+sex = pd.get_dummies(train['Sex'], drop_first=True)
+embark = pd.get_dummies(train['Embarked'], drop_first=True)
 
+# Drop unnecessary columns
+train.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)
+
+# Concatenate dummy variables
+train = pd.concat([train, sex, embark], axis=1)
+
+# Logistic Regression Model
+x_train, x_test, y_train, y_test = train_test_split(train.drop('Survived', axis=1), train['Survived'], test_size=0.30, random_state=101)
+logmodel = LogisticRegression()
+logmodel.fit(x_train, y_train)
+predictions = logmodel.predict(x_test)
 sc = MinMaxScaler(feature_range=(0, 1))
-X_scaled = sc.fit_transform(X)
 
-log_model = LogisticRegression(C=1)
-log_model.fit(X_scaled, y)
-
-pickle.dump(log_model, open("ml_model.sav", "wb"))
+# Evaluation
+# print(confusion_matrix(y_test, predictions))
+# print(classification_report(y_test, predictions))
+pickle.dump(logmodel, open("ml_model.sav", "wb"))
 pickle.dump(sc, open("scaler.sav", "wb"))
